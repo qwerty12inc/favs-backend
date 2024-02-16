17ae103eb368449bb9490803140c23620c9fc731
vr009 <slavarianov@yandex.ru>
2024-02-16 22:59:51 +0200

    add initial project structure

---
+++ cmd/main/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"os"
+)
+
+type ServiceConfig struct {
+	Port string
+}
+
+const defaultPort = "8080"
+
+func NewServiceConfig() ServiceConfig {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+	return ServiceConfig{
+		Port: port,
+	}
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	serviceCfg := NewServiceConfig()
+	e := echo.New()
+	e.GET("/health/status", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Api is up and running!")
+	})
+
+	return e.Start(":" + serviceCfg.Port)
+}
