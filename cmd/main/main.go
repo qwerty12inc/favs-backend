e0681e19cc196cf40bd5acaf403be5f89a12c0af
vr009 <slavarianov@yandex.ru>
2024-02-16 23:10:51 +0200

    add middlewares

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 	"os"
 )
@@ -34,6 +35,11 @@ func main() {
 func run() error {
 	serviceCfg := NewServiceConfig()
 	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.RequestID())
+
 	e.GET("/health/status", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Api is up and running!")
 	})
