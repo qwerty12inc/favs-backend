24a36924b47b199aba8ce1a8885297806940a608
vr009 <slavarianov@yandex.ru>
2024-02-24 23:51:07 +0200

    db: remove postgres logic
    It's replaced with firestore

--- db/migrations/000001_add-initial-schema.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id serial NOT NULL PRIMARY KEY,
-    login text NOT NULL UNIQUE,
-    password bytea NOT NULL,
-    is_active boolean NOT NULL DEFAULT true,
-    created_at timestamp default (now() at time zone 'utc')
-);
